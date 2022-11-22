ad2b20faf461755af4e2e14adf11f8082714c1d6
Morten Brudvik <morten@brudvik.com>
2022-11-22 09:41:22 +0100

    chore: Code cleanup

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -5,17 +5,17 @@ const events = ['mousedown', 'touchstart'];
 export const useClickOutside = <T extends HTMLElement = any>(handler: () => void, nodes: HTMLElement[]) => {
 
     useEffect(() => {
-        const listener = (event: any) => {
+        const eventHandler = (event: any) => {
             if (Array.isArray(nodes)) {
-                const shouldTrigger = nodes.every((node) => !!node && !event.composedPath().includes(node));
+                const shouldTrigger = nodes.every((node) => !!node && !node.contains(event?.target));
                 if (shouldTrigger) {
                     handler();
                 }
             }
         };
 
-        events.forEach((fn) => document.addEventListener(fn, listener));
+        events.forEach(event => document.addEventListener(event, eventHandler));
 
-        return () => events.forEach((fn) => document.removeEventListener(fn, listener));
+        return () => events.forEach(event => document.removeEventListener(event, eventHandler));
     }, [handler, nodes]);
 };
